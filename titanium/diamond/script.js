d06dacfe87274e79409f47b597b955df6ea7b9f1
Zelk Zephner <TechZelk@proton.me>
2026-06-17 18:21:29 -0600

    Update script.js

--- titanium/diamond/script.js
+++ titanium/diamond/script.js
@@ -1,2 +1,40 @@
 let test = "test";
 console.log(test);
+let fatman = prompt("please enter your browser name.");
+
+if (fatman === "Edge") {
+    alert('You\'ve got the Edge!');
+}
+else if(fatman === "edge") {
+    alert('You\'ve got the Edge!');
+}
+else if(fatman === 'Chrome'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'Opera'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'Safari'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'FireFox'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'chrome'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'opera')
+{
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'safari'){
+    alert( 'Okay we support these browsers too' );
+}
+else if(fatman === 'fireFox'){
+    alert( 'Okay we support these browsers too' );
+}
+else{
+    alert('We hope that this page looks ok!');
+}
+
+
